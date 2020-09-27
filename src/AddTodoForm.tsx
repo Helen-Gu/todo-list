94d662939811cbb9606e3f44022fd2122d00e312
Helen Gu <helengu1996@gmail.com>
2020-09-27 12:44:33 -0400

    first commit

---
+++ src/AddTodoForm.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+interface Props {
+    addTodo: AddTodo;
+}
+
+export const AddTodoForm: React.FC<Props> = ({ addTodo }) => {
+    const [text, setText] = useState('');
+    return (
+        <form action="">
+            <input type="text" name="" id="" onChange={e => {
+                setText(e.target.value);
+            }} />
+            <button type="submit" onClick={e => {
+                e.preventDefault();
+                addTodo(text);
+                setText('');
+            }}>Add Todo</button>
+        </form>
+    );
+};
